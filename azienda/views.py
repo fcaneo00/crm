d6dd43ae06f5b21102dbbd5f5b6950b2a6fa0984
fcaneo00 <filippo.caneo@gmail.com>
2022-10-19 10:47:59 +0200

    for loop in azienda.html

--- azienda/views.py
+++ azienda/views.py
@@ -1,9 +1,14 @@
+from .models import collaboratore
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    collaboratori = collaboratore.objects.all().values()
     template = loader.get_template('azienda.html')
-    return HttpResponse(template.render())
+    context = {
+        "collaboratori": collaboratori,
+    }
+    return HttpResponse(template.render(context, request))
     
